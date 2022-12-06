2480873b2c73926e7cd3fad9c3c579bd18e73c58
Jonathan Burgos Saldivia <jonathanburgoss@outlook.com>
2022-12-06 17:44:54 -0300

    Update install_anydesk.sh

--- install_anydesk.sh
+++ install_anydesk.sh
@@ -8,4 +8,8 @@ makepkg -si
 cd /tmp
 wget https://download.anydesk.com/linux/anydesk-6.1.0-amd64.tar.gz
 tar -xvzf anydesk-6.1.0-amd64.tar.gz
-cd anydesk-6.1.0/
+sudo mv /tmp/anydesk-*/anydesk /usr/local/bin
+sudo chown -R root:root /usr/local/bin/anydesk
+sudo chmod 777 /usr/local/bin/anydesk
+
+anydesk
